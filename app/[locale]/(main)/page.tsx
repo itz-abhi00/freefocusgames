98a22c1a3e3750cfc8e2078ed91ba2079f8b538c
itz-abhi00 <abhijeetgiri.abhi@gmail.com>
2026-09-15 21:00:44 +0530

    Update page.tsx

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@/i18n/navigation";
-import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { setRequestLocale } from 'next-intl/server';
 import type { Metadata } from "next";
 
 export const dynamic = "force-static";
@@ -36,7 +36,6 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
                     Are you faster than your friends?
                 </p>
 
-                {/* Make sure the href matches the exact URL of the stroop game in this template. Usually it is /games/stroop-test or /games/stroop-task */}
                 <Link href="/games/stroop-task"> 
                     <button className="text-2xl md:text-3xl font-bold px-12 py-6 bg-[#222] border-2 border-[#00ff41] text-[#00ff41] uppercase shadow-[6px_6px_0px_#00ff41] hover:translate-y-[2px] hover:translate-x-[2px] hover:shadow-[4px_4px_0px_#00ff41] transition-all">
                         INSERT COIN TO PLAY
