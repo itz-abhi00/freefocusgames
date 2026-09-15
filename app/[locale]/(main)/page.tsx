bf710fd1f8c8a891f61263220aeac5c821bdc98a
itz-abhi00 <abhijeetgiri.abhi@gmail.com>
2026-09-15 21:05:36 +0530

    Update page.tsx

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -36,7 +36,7 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
                     Are you faster than your friends?
                 </p>
 
-                <Link href="/games/stroop-task"> 
+                <Link href="/games/stroop-effect-test"> 
                     <button className="text-2xl md:text-3xl font-bold px-12 py-6 bg-[#222] border-2 border-[#00ff41] text-[#00ff41] uppercase shadow-[6px_6px_0px_#00ff41] hover:translate-y-[2px] hover:translate-x-[2px] hover:shadow-[4px_4px_0px_#00ff41] transition-all">
                         INSERT COIN TO PLAY
                     </button>
